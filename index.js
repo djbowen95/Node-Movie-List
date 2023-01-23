89f5e7821479b282d6c4c10478b64a5b8e166d05
Daniel Bowen <djbowen95@gmail.com>
2023-01-23 22:42:00 +0000

    Remove try/catch from function

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 const processArgv = process.argv; // Import/store process arguments.
 
 const netflixQueue = {
-    queue: [
-      "Mr. Nobody",
-      "The Matrix",
-      "Eternal Sunshine of the Spotless Mind",
-      "Fight Club"
-    ],
-    watchMovie: function () {
-      this.queue.pop();
-    },
-    addMovie: function (movie) {
-      this.queue.unshift(movie);
-    },
-    printQueue: function () {
-      var list = "";
-      for (var i = this.queue.length - 1; i >= 0; i--) {
-        var currentMovie = this.queue[i];
-        list += (this.queue.length - i) + ". " + currentMovie + "\n";
-      }
-      console.log(list);
+  queue: [
+    "Mr. Nobody",
+    "The Matrix",
+    "Eternal Sunshine of the Spotless Mind",
+    "Fight Club",
+  ],
+  watchMovie: function () {
+    this.queue.pop();
+  },
+  addMovie: function (movie) {
+    this.queue.unshift(movie);
+  },
+  printQueue: function () {
+    var list = "";
+    for (var i = this.queue.length - 1; i >= 0; i--) {
+      var currentMovie = this.queue[i];
+      list += this.queue.length - i + ". " + currentMovie + "\n";
     }
-  };
+    console.log(list);
+  },
+};
 
 const parseArgv = (argv) => {
-    try {
-        if (argv[2] === "watch") {
-            console.log("Watch");
-        }
-        if (argv[2] === "add") {
-            console.log("Add");
-        }
-    } catch (err) { // Doesn't catch this as an error either. 
-        console.log("Caught err: ");
-        console.log(err);
-    }
-}
+  let command = argv[2].toLowerCase(); // Make lowercase.
+  let title = argv[3]; // Want this to include everything after 3.
+  if (command === "watch") {
+    console.log("Watch");
+  }
+  if (command === "add") {
+    console.log("Add");
+  }
+};
 
 parseArgv(processArgv);
